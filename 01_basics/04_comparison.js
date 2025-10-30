c6d84ef6d04604de8b0cf4e7ffeec7f03ece888b
chandankashyap2744-stack <chandankashyap2744@gmail.com>
2025-10-30 11:36:04 +0000

    comparison of datatypes

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,31 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+
+console.log("2">1);
+console.log("02">1);
+
+console.log(null > 0);
+console.log(null == 0); 
+console.log(null >= 0); //null converted into 0 cause these operators works differently generally avoid these type of comparison
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// ===
+
+console.log("2" === 2);
+
+
+
+
+
+
+
+
+
+
