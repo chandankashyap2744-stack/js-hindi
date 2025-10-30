ba380388612c75e3b233ee20b1d45567d6c34b41
chandankashyap2744-stack <chandankashyap2744@gmail.com>
2025-10-30 09:58:57 +0000

    datatypes

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,34 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3 + 3) // We ar using nodeJs, not browser
+
+ console.log(3
+    +3); //code readability should we high
+
+
+console.log("Chandan")
+
+let name = "Chandan"
+let age = "18"
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique   
+
+// object
+
+console.log(typeof "Chandan");
+
+console.log(typeof age);
+
+console.log(typeof null); //object
+
+console.log(typeof undefined); // undefned
+
+
+
