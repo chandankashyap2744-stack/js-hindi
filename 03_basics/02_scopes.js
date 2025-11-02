121168a8c7c71b90636ec37300a5cf368b4756fc
chandankashyap2744-stack <chandankashyap2744@gmail.com>
2025-11-02 13:47:09 +0000

    scopes

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,18 @@
+
+
+if(true){
+    let a = 10
+    const b = 20
+    var c = 30
+}
+
+// console.log(a);
+// console.log(b);
+console.log(c);
+
+
+for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+}
+
+// these curly braces define a block scope
